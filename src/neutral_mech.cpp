69fffd7a9a8821245d867c316f5bbcfb1eb50781
tfninjadoom <tfninjadoom@gmail.com>
2024-10-29 16:50:34 -0700

    caps neutral at 75% speed

--- src/neutral_mech.cpp
+++ src/neutral_mech.cpp
@@ -9,14 +9,20 @@ pros::Motor    neutralMotor(0);
 void initPID(double Kp, double Ki, double Kd, bool resetSensor) {
     if (resetSensor) { neutralRotation.reset_position(); }
     
-    //PID_AntiWindup neutralPID(1,0,1,1);
+    double min = -9000, max = 9000; //75% of motor speed
     SimplePID neutralPID(1,0,1);
+    //PID_AntiWindup neutralPID(1,0,1,1);
 
     pros::Task neutralTask(
         [&]()->void {
             double currentState { (double)neutralRotation.get_position() };
             double output { neutralPID.calculate(Neutral::target, currentState) };
-            neutralMotor.move_voltage(output);
+
+            // clamps value to min & max
+            const double temp = output < min ? min : output;
+            output = temp > max ? max : output;
+
+            neutralMotor.move_voltage(output); //input range of +-12000mV
             pros::delay(10);
         }
     );
