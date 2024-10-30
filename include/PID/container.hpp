fc78a10b64d41c21cf3189e5c241a9f357e7f392
tfninjadoom <tfninjadoom@gmail.com>
2024-10-29 17:00:11 -0700

    PID class inheritance (incomplete)

--- include/PID/container.hpp
+++ include/PID/container.hpp
@@ -5,7 +5,43 @@
 #include "PID/integral.hpp"
 #include "PID/derivative.hpp"
 
+class PID_Base {
 
+public:
+    PID_Base(double Kp, double Ki, double Kd);
+
+    virtual double calculate(double target, double currentState);
+
+protected:
+    double m_Kp, m_Ki, m_Kd;
+    double m_error;
+
+    std::unique_ptr<ProportionalBase>   m_P;
+    std::unique_ptr<IntegralBase>       m_I;
+    std::unique_ptr<DerivativeBase>     m_D;
+
+};
+
+class PID_Derived : PID_Base {
+
+public:
+    PID_Derived(double Kp, double Ki, double Kd, double value);
+
+    double calculate(double target, double currentState) override;
+
+};
+
+class PID_AntiWindup : PID_Base {
+
+public:
+    PID_AntiWindup(double Kp, double Ki, double Kd, double maxWindup);
+
+    double calculate(double target, double currentState) override;
+
+private:
+    double m_max;
+
+};
 
 //---------------------------------- SimplePID ------------------------------------//
 class SimplePID {
--- src/PID/container.cpp
+++ src/PID/container.cpp
@@ -1,7 +1,37 @@
 // container.cpp
 #include "PID/container.hpp"
 
+PID_Base::PID_Base(double Kp, double Ki, double Kd) :
+    m_Kp(Kp), m_Ki(Ki), m_Kd(Kd), m_error() //nullptr
+{}
 
+double PID_Base::calculate(double target, double currentState) {
+    
+    return (m_Kp * m_P->calculate() + m_Ki * m_I->calculate() + m_Kd * m_D->calculate());
+}
+
+PID_Derived::PID_Derived(double Kp, double Ki, double Kd, double value) :
+    PID_Base::PID_Base(Kp, Ki, Kd)
+{
+    m_P = std::make_unique<ProportionalDerived>(value);
+    m_I = std::make_unique<IntegralDerived>(value);
+    m_D = std::make_unique<DerivativeDerived>(value);
+}
+
+double PID_Derived::calculate(double target, double currentState) {
+    // do stuff with m_value and m_member
+    return 0.;
+}
+
+PID_AntiWindup::PID_AntiWindup(double Kp, double Ki, double Kd, double maxWindup) :
+    PID_Base::PID_Base(Kp, Ki, Kd), m_max(maxWindup)
+{
+    return;
+}
+
+double PID_AntiWindup::calculate(double target, double currentState) {
+    return 0.;
+}
 
 //---------------------------------- SimplePID ------------------------------------//
 SimplePID::SimplePID(double Kp, double Ki, double Kd) :
